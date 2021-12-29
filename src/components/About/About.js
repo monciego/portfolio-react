20281a46a57e3cabd01869d290e1ec8eb238bd4b
Jericho <jerichobantiquete@gmail.com>
2021-12-29 18:07:27 +0800

    about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,55 @@
 import React from "react";
-import { StyledAbout } from "../styles/About.styled";
+import {
+  AboutContent,
+  AboutDetailsContainer,
+  AboutImage,
+  AboutImageContainer,
+  Resume,
+  ResumeLink,
+  StyledAbout,
+} from "../styles/About.styled";
 import { SectionHeading, SectionTitle } from "../styles/SectionHeading";
+import { StyledParagraph } from "../styles/Typography.styled";
+import myImage from "../../assets/my-image.jpg";
+import downloadIcon from "../../assets/download-icon.svg";
 
 const About = () => {
   return (
     <StyledAbout id="about">
-      <SectionHeading dark="true">
+      <SectionHeading dark="true" mb="3rem">
         <SectionTitle dark="true" number="02">
           About Me
         </SectionTitle>
       </SectionHeading>
+      <AboutContent>
+        <AboutImageContainer>
+          <AboutImage src={myImage} />
+        </AboutImageContainer>
+        <AboutDetailsContainer>
+          <StyledParagraph>
+            My name is Jericho P. Bantiquete, currently pursuing Bachelor of
+            Science in Information Technology in Pangasinan State University. I
+            am a passionate front-end developer based in Philippines.
+          </StyledParagraph>
+          <StyledParagraph>
+            I love to design and create responsive websites or web apps from
+            scratch. The technologies, tools and languages I am using to build
+            my projects are HTML, CSS, JavaScript, REACT JS, TailwindCSS,
+            Bootstrap, PHP Laravel, Git and GitHub, Figma, Adobe XD and VS Code.
+          </StyledParagraph>
+          <StyledParagraph>
+            I have eagerness to learn something new every day and I love to
+            share my knowledge to my fellow students and developers. And I am
+            looking forward to talk with you!
+          </StyledParagraph>
+
+          <Resume>
+            <ResumeLink href="/" icon={downloadIcon}>
+              Download Resume
+            </ResumeLink>
+          </Resume>
+        </AboutDetailsContainer>
+      </AboutContent>
     </StyledAbout>
   );
 };
--- src/components/styles/About.styled.js
+++ src/components/styles/About.styled.js
@@ -4,3 +4,131 @@ export const StyledAbout = styled.section`
   background-color: ${({ theme }) => theme.backgroundColor.dark};
   color: ${({ theme }) => theme.color.lightGray};
 `;
+
+export const AboutContent = styled.div`
+  display: flex;
+  gap: 5%;
+  margin-top: 6.5rem;
+
+  @media all and (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+    flex-direction: column;
+    margin-top: 0;
+  }
+`;
+
+export const AboutImageContainer = styled.div`
+  position: relative;
+  width: 45%;
+  height: 28rem;
+  z-index: 3;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: -1rem;
+    left: -1rem;
+    background: #fcdf3e;
+    width: 100%;
+    height: 100%;
+    border-radius: 1px;
+    z-index: -2;
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    inset: 0 0 0 0;
+    background: rgb(13, 14, 14, 0.199);
+    z-index: 2;
+  }
+
+  @media all and (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+    width: 100%;
+    height: 20rem;
+    margin-bottom: 1.75rem;
+
+    &::after {
+      content: "";
+      top: -0.5rem;
+      left: -0.5rem;
+    }
+  }
+`;
+
+export const AboutImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  position: relative;
+  border-radius: 1px;
+`;
+
+export const AboutDetailsContainer = styled.div`
+  position: relative;
+  width: 50%;
+
+  @media all and (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+    width: 100%;
+  }
+`;
+
+export const Resume = styled.div`
+  display: inline-block;
+  margin-top: 3rem;
+  position: relative;
+  transition: 0.3s ease;
+
+  &::after {
+    content: "";
+    display: block;
+    position: absolute;
+    right: 0;
+    left: 0;
+    bottom: -0.5rem;
+    height: 1px;
+    border-radius: 5px;
+    background: ${({ theme }) => theme.backgroundColor.light};
+    width: 100%;
+    transition: transform 0.3s ease;
+    transform: scaleX(0);
+    transform-origin: bottom right;
+  }
+
+  &:hover::after {
+    transform: scaleX(1);
+    transform-origin: bottom left;
+  }
+
+  @media all and (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+    margin-top: 1rem;
+  }
+`;
+
+export const ResumeLink = styled.a`
+  color: #fff;
+  letter-spacing: 2px;
+  font-weight: 600;
+  cursor: pointer;
+
+  &::before {
+    content: ${(props) => `url(${props.icon})`};
+    position: absolute;
+    right: -2rem;
+    opacity: 0;
+    filter: alpha(opacity=0);
+    -webkit-transform: translateX(-20px);
+    transform: translateX(-20px);
+    transition: all 0.3s;
+  }
+
+  &:hover::before {
+    opacity: 1;
+    filter: alpha(opacity=100);
+    -webkit-transform: translateX(0);
+    transform: translateX(0);
+  }
+
+  @media all and (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+    font-size: 1rem;
+  }
+`;
